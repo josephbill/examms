f9ad2a59574448737044f85991d4be9524f42aec
josephbill <josephbill>
2017-06-08 16:44:29 -0700

    added login and register

---
+++ app/src/main/java/com/example/josephbill/examms/adapters/UsersRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.josephbill.examms.adapters;
+
+/**
+ * Created by Joseph Bill on 6/8/2017.
+ */
+
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.josephbill.examms.R;
+import com.example.josephbill.examms.modal.user;
+
+import java.util.List;
+
+public class UsersRecyclerAdapter extends RecyclerView.Adapter<UsersRecyclerAdapter.UserViewHolder> {
+
+    private List<user> listUsers;
+
+    public UsersRecyclerAdapter(List<user> listUsers) {
+        this.listUsers = listUsers;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflating recycler item view
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_user_recycle, parent, false);
+
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        holder.textViewName.setText(listUsers.get(position).getName());
+        holder.textViewEmail.setText(listUsers.get(position).getEmail());
+        holder.textViewPassword.setText(listUsers.get(position).getPassword());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.v(UsersRecyclerAdapter.class.getSimpleName(),""+listUsers.size());
+        return listUsers.size();
+    }
+
+
+    /**
+     * ViewHolder class
+     */
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public AppCompatTextView textViewName;
+        public AppCompatTextView textViewEmail;
+        public AppCompatTextView textViewPassword;
+
+        public UserViewHolder(View view) {
+            super(view);
+            textViewName = (AppCompatTextView) view.findViewById(R.id.textViewName);
+            textViewEmail = (AppCompatTextView) view.findViewById(R.id.textViewEmail);
+            textViewPassword = (AppCompatTextView) view.findViewById(R.id.textViewPassword);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/josephbill/examms/modal/user.java
@@ -0,0 +1,44 @@
+package com.example.josephbill.examms.modal;
+
+/**
+ * Created by Joseph Bill on 6/8/2017.
+ */
+
+public class user {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
